e4b56606dc70d2ae3f3ad508fa61363980c3cc64
alex - windows <alicelukas@naver.com>
2020-06-28 22:50:41 +0900

    message.js 개선

--- events/message.js
+++ events/message.js
@@ -1,12 +1,8 @@
-const prefix = '.';
-
 module.exports = async (bot, message) => { 
-    if(message.author.bot) return;
+    if(message.author.bot || !message.content.startsWith(bot.prefix)) return;
 
-    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let args = message.content.slice(bot.prefix.length).trim().split(/ +/g);
     let command = args.shift().toLowerCase();
-
-    if(!message.content.startsWith(prefix)) return;
     let cmd = bot.commands.get(command)
     if(cmd) cmd.run(message, args)
 }
